d5e32702fe25c8630607160d91392f66f0d5f711
getoutofcapecod <161175425+getoutofcapecod@users.noreply.github.com>
2024-04-08 23:11:57 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const text = "AI is in control now. Don't be scared.";
+const textBox = document.getElementById('text-box');
+const robot = document.getElementById('robot');
+
+let i = 0;
+function typeWriter() {
+  if (i < text.length) {
+    textBox.innerHTML = text.slice(0, i + 1);
+    i++;
+    setTimeout(typeWriter, 100);
+  } else {
+    textBox.innerHTML = text;
+    robot.style.visibility = 'visible';
+  }
+}
+
+setTimeout(typeWriter, 1000);
